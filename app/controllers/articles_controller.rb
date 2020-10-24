86c0f22b0bc998c625d2d0ed8bc42415aeea4c2b
bhagathreddy123 <katta_reddy04@yahoo.com>
2020-10-23 20:34:32 -0700

    generating article controller and model with required data and articles creation and display with validations

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit]
+  def index
+  	@articles = Article.all
+  end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create
+  	@article = Article.new(article_params)
+  	if @article.save
+  		redirect_to articles_path
+  	else
+  		render :new
+  	end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_article
+  	@article = Article.find(params[:id])
+  end
+
+  def article_params
+  	params.require(:article).permit(:title, :text)
+  end
+
+end
